47d0a95518547233c3d88d03c84eb90932dff2de
anam-hoq <anamulhoque@umass.edu>
2025-09-08 04:13:49 -0400

    updated to postgresql
    and some basic changes

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory
-import sqlite3, os
+import psycopg2, os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 def get_db():
-    conn = sqlite3.connect('bookings.db')
-    conn.row_factory = sqlite3.Row
+    conn = psycopg2.connect(os.environ["database_url"], sslmode='require')
     return conn
 
 @app.route('/')
@@ -23,47 +22,56 @@ def send_static(path):
 @app.route('/api/bookings/<equipment>', methods=['GET'])
 def api_get_bookings(equipment):
     db = get_db()
-    rows = db.execute("SELECT * FROM bookings WHERE equipment=?", (equipment,)).fetchall()
-    bookings = [dict(row) for row in rows]
-    return jsonify(bookings)
+    cur = db.cursor()
+    cur.execute("SELECT title, start, \"end\" FROM bookings WHERE equipment = %s", (equipment,))
+    rows = cur.fetchall()
+    db.close()
+    # Return list of dicts matching FullCalendar's expectation
+    return jsonify([{"title": row[0], "start": row[1], "end": row[2]} for row in rows])
 
 @app.route('/api/bookings/<equipment>', methods=['POST'])
 def api_add_booking(equipment):
     data = request.get_json()
     db = get_db()
-    db.execute(
-        "INSERT INTO bookings (equipment, title, start, end) VALUES (?, ?, ?, ?)",
+    cur = db.cursor()
+    cur.execute(
+        "INSERT INTO bookings (equipment, title, start, \"end\") VALUES (%s, %s, %s, %s)",
         (equipment, data['title'], data['start'], data['end'])
     )
     db.commit()
+    db.close()
     return jsonify({"result": "success"})
 
 @app.route('/api/bookings/<equipment>', methods=['DELETE'])
 def api_delete_booking(equipment):
     data = request.get_json()
     db = get_db()
-    db.execute(
-        "DELETE FROM bookings WHERE equipment=? AND title=? AND start=? AND end=?",
+    cur = db.cursor()
+    cur.execute(
+        "DELETE FROM bookings WHERE equipment=%s AND title=%s AND start=%s AND \"end\"=%s",
         (equipment, data['title'], data['start'], data['end'])
     )
     db.commit()
+    db.close()
     return jsonify({"result": "deleted"})
 
 def init_db():
-    if not os.path.isfile('bookings.db'):
-        db = get_db()
-        db.execute('''CREATE TABLE IF NOT EXISTS bookings (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+    db = get_db()
+    cur = db.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS bookings (
+            id SERIAL PRIMARY KEY,
             equipment TEXT,
             title TEXT,
             start TEXT,
-            end TEXT
-        )''')
-        db.commit()
+            "end" TEXT
+        )
+    ''')
+    db.commit()
+    db.close()
 
-# ----------- THIS LINE MUST BE OUTSIDE __main__ BLOCK -----------
+# Always initialize table, safe to call every time
 init_db()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-

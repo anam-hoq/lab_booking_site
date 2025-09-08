b0cf16980a098deef9ab372dbbe0ad45088f0e18
anam-hoq <anamulhoque@umass.edu>
2025-09-08 03:27:23 -0400

    init db
    db is initialiez outside main

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def init_db():
             end TEXT
         )''')
         db.commit()
-
+init_db() 
 if __name__ == '__main__':
     init_db()
     app.run(host='0.0.0.0', port=5000, debug=True)
